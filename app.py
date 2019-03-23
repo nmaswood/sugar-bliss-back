fac3e2ffe00754d811d7ae185b203ea483fc6626
nmaswood <nmaswood@civisanalytics.com>
2019-03-23 15:31:46 -0700

    bar

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 from chalice import Chalice
-from chalicelib.sugar_bliss_backend import calculate
-from pdb import set_trace
-
+from chalicelib.sugar_bliss_backend import calculate, constants
 
 app = Chalice(app_name='sugar-bliss-backend')
 app.debug = True
 
 
-@app.route('/', methods=('GET',), cors=True)
-def ping():
-    return 'pong'
+@app.route('/data', methods=('GET', ), cors=True)
+def data():
+    return constants.DATA
 
 
-@app.route('/submit', methods=('POST',), cors=True)
+@app.route('/submit', methods=('POST', ), cors=True)
 def submit():
     json_obj = app.current_request.json_body
     errors = calculate.validate(json_obj)
 
     if errors:
-        return {
-            'status': 'fail',
-            'errors': errors
-        }
+        return {'status': 'fail', 'errors': errors}
 
     preprocessed = calculate.preprocess(json_obj)
     food_obj, time_obj = calculate.split_data(preprocessed)
